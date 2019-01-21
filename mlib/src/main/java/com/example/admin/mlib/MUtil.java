aec399f10433eb76a8417844238c0d93cbd06694
ws <1175990110@qq.com>
2019-01-21 18:26:07 +0800

    Initial commit

---
+++ mlib/src/main/java/com/example/admin/mlib/MUtil.java
@@ -0,0 +1,13 @@
+package com.example.admin.mlib;
+
+import android.util.Log;
+
+/**
+ * created by ws on 2019/1/21 18:21
+ */
+public class MUtil {
+    public static void addTest(){
+        Log.e("mtest", "addTest: ---" );
+
+    }
+}
